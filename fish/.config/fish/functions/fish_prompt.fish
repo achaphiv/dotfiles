3b8de04048391c2d2cf0af059e7cc4ed46a54ee7
Alex Chaphiv <achaphiv@gmail.com>
2016-07-09 13:38:50 -0400

    Change fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,33 +1,47 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+	#Save the return status of the previous command
+    set stat $status
 
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
+# Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
 
-  echo -n '@'
+if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
 
-  # Host
-  set_color $fish_color_host
-  echo -n (hostname -s)
-  set_color normal
+if not set -q __fish_color_blue
+        set -g __fish_color_blue (set_color -o blue)
+    end
 
-  echo -n ':'
+#Set the color for the status depending on the value
+    set __fish_color_status (set_color -o green)
+    if test $stat -gt 0
+        set __fish_color_status (set_color -o red)
+    end
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
+switch $USER
 
-  __terlar_git_prompt
-#  __fish_hg_prompt
-  echo
+case root toor
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
+if not set -q __fish_prompt_cwd
+            if set -q fish_color_cwd_root
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            else
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+        end
 
-  echo -n '➤ '
-  set_color normal
+printf '%s@%s %s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
+
+case '*'
+
+if not set -q __fish_prompt_cwd
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+
+printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal"
+
+end
 end
