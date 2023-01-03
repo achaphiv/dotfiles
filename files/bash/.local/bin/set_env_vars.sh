1e5653ab941e7522f8b824a76c42530af3fb2237
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2023-01-03 13:02:01 -0500

    feat(bash): remove commented lines

--- files/bash/.local/bin/set_env_vars.sh
+++ files/bash/.local/bin/set_env_vars.sh
@@ -2,14 +2,6 @@
 
 APPS=$HOME/.local/opt
 
-#export ANT_HOME=$APPS/apache-ant/
-#export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
-#export JAVA_HOME=$APPS/openjdk1.7/
-#export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
-#export _JAVA_OPTIONS="-Xmx4g -XX:MaxPermSize=1g"
-
-#export ANDROID_HOME=$APPS/android-sdks/
-
 export PATH=$(paste -d ":" -s - << EOF
 $HOME/.local/bin/
 $PATH
@@ -24,13 +16,3 @@ export DEBEMAIL="no1wantdthisname@gmail.com"
 
 ulimit -c unlimited
 ulimit -Sn unlimited || true
-
-# less colors
-#export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-#export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
-#export LESS_TERMCAP_me=$'\E[0m'           # end mode
-#export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-#export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
-#export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-#export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
-#export LESS=-r
