58292f90a587a8c7d2c78eeadd8bb009b1799f5c
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2023-01-03 13:02:21 -0500

    feat(bash): remove environment variables for ppa packaging

--- files/bash/.local/bin/set_env_vars.sh
+++ files/bash/.local/bin/set_env_vars.sh
@@ -11,8 +11,5 @@ EOF
 export FISH_CLIPBOARD_CMD=cat
 export TERM=xterm-256color
 
-export DEBFULLNAME="Bob Chez"
-export DEBEMAIL="no1wantdthisname@gmail.com"
-
 ulimit -c unlimited
 ulimit -Sn unlimited || true
