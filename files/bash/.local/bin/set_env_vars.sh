e4e20d42c94945c4b84d46c02fbae8dd93ba9a71
Alex Chaphiv <achaphiv@gmail.com>
2023-01-10 01:00:33 -0500

    fix(bash): avoid overriding already set `$SSH_AUTH_SOCK`

--- files/bash/.local/bin/set_env_vars.sh
+++ files/bash/.local/bin/set_env_vars.sh
@@ -14,7 +14,7 @@ ${XDG_DATA_DIRS:-/usr/local/share:/usr/share}
 EOF
 )
 
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+export SSH_AUTH_SOCK="${SSH_AUTH_SOCK:-${XDG_RUNTIME_DIR}/ssh-agent.socket}"
 
 export FISH_CLIPBOARD_CMD=cat
 export TERM=xterm-256color
